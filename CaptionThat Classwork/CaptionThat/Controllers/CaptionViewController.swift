0d88ae2585eef807b4a219a7de8f3c5e21f9791e
masher1 <masher@syr.edu>
2020-02-27 20:43:58 -0500

    Updated Assignment 2

--- CaptionThat Classwork/CaptionThat/Controllers/CaptionViewController.swift
+++ CaptionThat Classwork/CaptionThat/Controllers/CaptionViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class CaptionViewController: UIViewController, UITextFieldDelegate {
+class CaptionViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
     //MARK: Properties
-    @IBOutlet weak var userImageView: UIImageView!
     @IBOutlet weak var captionTextField: UITextField!
     @IBOutlet weak var captionValueLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Handle the text field’s user input through delegate callbacks.
@@ -36,11 +36,42 @@ class CaptionViewController: UIViewController, UITextFieldDelegate {
         captionValueLabel.text = textField.text
     }
     
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        // Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+
+    
     /// Mark: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         captionValueLabel.text = "Default Text"
     }
     
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // Hide the keyboard.
+        captionTextField.resignFirstResponder()
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
     
     
     /*
