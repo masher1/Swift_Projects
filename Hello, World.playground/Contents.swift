ac5458e2fc507fe52c21c8929e55f5cd434629f1
Malkiel Asher <masher@syr.edu>
2020-03-26 03:34:06 -0400

    Uploaded AboutMe and RainbowTabs

---
+++ Hello, World.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print(str)
+
+print("I have learned the following:")
+print("How to use Playgrouds to view simple print statements")
+print("What features make Swift a modern and safe language")
+print("How to use the Swift REPL in Terminal")
+print("How to use playgrounds to make writing Swift fun and simple")
+
+
+print("Otherside - Avi Kaplan")
+
+print("Emerald ivy")
+print("Wrapped around my skin")
+
+print("Pulls me deeper")
+print("Drags me back again")
+
+print("Tangle me up in your vines")
+print("Take my spirit, take my mind")
+print("Take me over to that other side")
---
+++ RainbowTabs/RainbowTabs/RedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by Malkiel Asher on 3/25/20.
+//  Copyright © 2020 Malkiel Asher. All rights reserved.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //tabBarItem.badgeValue = "!"
+        //tabBarItem.badgeValue = nil
+    }
+
+
+}
+
